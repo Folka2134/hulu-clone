be82b23b9e41257e3eef083aa07022704f6bc7c4
Folka2134 <folka2134@gmail.com>
2022-05-10 21:32:52 +0100

    Created HeaderItem component

---
+++ components/HeaderItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HeaderItem = ({ title, Icon }) => {
+  return (
+    <div className='flex flex-col items-center w-12 sm:w-20 cursor-pointer hover:text-white group'>
+      <Icon className='h-8 mb-1 group-hover:animate-bounce'/>
+      <p className='opacity-0 group-hover:opacity-100 tracking-widest'>{title}</p>
+    </div>
+  )
+}
