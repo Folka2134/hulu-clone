688adf852ea9b59362bca61d67ebe47d83a9c410
Folka2134 <folka2134@gmail.com>
2022-05-11 12:50:13 +0100

    Functional page

--- components/Results.js
+++ components/Results.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Thumbnail } from './Thumbnail'
 
 export const Results = ({ results }) => {
-  // console.log(results);
   return (
     <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:flex flex-wrap justify-center'>
       {results.map((result) => (
